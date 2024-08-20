93780d60756250c16f92045cec12e18067365657
Gustavo da Costa Fontana <gucfontana@gmail.com>
2024-08-20 16:57:50 -0300

    Add project files.

---
+++ LocadoraWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LocadoraWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
