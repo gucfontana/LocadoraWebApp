d627e921002e0a8a7c0803327f33dce7cfca2efc
Gustavo da Costa Fontana <gucfontana@gmail.com>
2024-08-28 16:37:51 -0300

    arquivos migrations

---
+++ Locadora.Infra/Migrations/20240828170102_TBClientes.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Locadora.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class TBClientes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBClientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Email = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Telefone = table.Column<string>(type: "varchar(15)", nullable: false),
+                    TipoCadastroCliente = table.Column<int>(type: "int", nullable: false),
+                    NumeroDocumento = table.Column<string>(type: "varchar(20)", nullable: false),
+                    Cidade = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Estado = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Cep = table.Column<string>(type: "varchar(10)", nullable: false),
+                    Rua = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Bairro = table.Column<string>(type: "varchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBClientes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBClientes");
+        }
+    }
+}
