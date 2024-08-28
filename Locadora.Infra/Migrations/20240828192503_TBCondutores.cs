6fe302c56eb034ebbeb123d07c2b6395c28e153f
Gustavo da Costa Fontana <gucfontana@gmail.com>
2024-08-28 16:37:36 -0300

    arquivos migrations

---
+++ Locadora.Infra/Migrations/20240828192503_TBCondutores.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Locadora.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class TBCondutores : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBCondutores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClienteId = table.Column<int>(type: "int", nullable: false),
+                    ClienteCondutor = table.Column<bool>(type: "bit", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Email = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Telefone = table.Column<string>(type: "varchar(20)", nullable: false),
+                    CPF = table.Column<string>(type: "varchar(20)", nullable: false),
+                    CNH = table.Column<string>(type: "varchar(20)", nullable: false),
+                    ValidadeCNH = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBCondutores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TBCondutores_TBClientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "TBClientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBCondutores_ClienteId",
+                table: "TBCondutores",
+                column: "ClienteId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBCondutores");
+        }
+    }
+}
