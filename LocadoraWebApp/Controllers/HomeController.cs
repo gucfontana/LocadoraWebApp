81c71a46e2b093cc909ac32eb519739317ec71fa
Gustavo da Costa Fontana <gucfontana@gmail.com>
2024-09-06 17:07:43 -0300

    remake layout

--- LocadoraWebApp/Controllers/HomeController.cs
+++ LocadoraWebApp/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using LocadoraWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 
-namespace LocadoraWebApp.Controllers
-{
+namespace LocadoraWebApp.Controllers;
+
+[Authorize(Roles = "Empresa,Funcionario")]
     public class HomeController : Controller
     {
 
@@ -13,4 +15,4 @@ namespace LocadoraWebApp.Controllers
         }
 
     }
-}
+
